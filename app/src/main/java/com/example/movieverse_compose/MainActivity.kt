da4d7ba0cf39bf4e78a8f47b97b8e24fd6e567db
Muhammad Ali <90325088+Ali0092@users.noreply.github.com>
2025-01-21 00:08:22 +0500

    added initial views

--- app/src/main/java/com/example/movieverse_compose/MainActivity.kt
+++ app/src/main/java/com/example/movieverse_compose/MainActivity.kt
@@ -3,23 +3,39 @@ package com.example.movieverse_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
-import androidx.compose.ui.platform.ComposeView
-import com.example.movieverse_compose.ui.theme.MovieVerseComposeTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(ComposeView(this).apply {
-            setContent {
-                Text(text = "Hello Universe : )")
-            }
-        })
-//        setContent {
-//            MovieVerseComposeTheme {
-//
-//            }
-//        }
+        setContent {
+            Greetings()
+        }
     }
 }
+
+@Composable
+fun Greetings() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Hello Universe :)")
+    }
+}
+
+//This preview will ruin the system's performance... so lets not use this....
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    MovieVerseComposeTheme {
+//        Greetings()
+//    }
+//}
