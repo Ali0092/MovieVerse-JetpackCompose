d620f916a865fe5b77933f90cded932529bf9175
Muhammad Ali <90325088+Ali0092@users.noreply.github.com>
2025-01-31 12:54:31 +0500

    Resolved again data loading issue of main screen

--- app/src/main/java/com/example/movieverse_compose/presentation/MainScreenViewModel.kt
+++ app/src/main/java/com/example/movieverse_compose/presentation/MainScreenViewModel.kt
@@ -1,12 +1,6 @@
 package com.example.movieverse_compose.presentation
 
-
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.movieverse_compose.common.ViewState
@@ -19,11 +13,11 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 
-class MainScreenViewModel (
+class MainScreenViewModel(
     private val getPopularMoviesListUseCase: GetPopularMoviesListUseCase,
     private val getUpcomingMoviesListUseCase: GetUpcomingMoviesListUseCase,
     private val getTVShowsListUseCase: GetTVShowsListUseCase,
-    ): ViewModel() {
+) : ViewModel() {
 
     private val _popularMovies = MutableStateFlow(MoviesViewState())
     val popularMovies: MutableStateFlow<MoviesViewState> = _popularMovies
@@ -34,23 +28,11 @@ class MainScreenViewModel (
     private val _tvShows = MutableStateFlow(MoviesViewState())
     val tvShows: MutableStateFlow<MoviesViewState> = _tvShows
 
-//    private var _selectedMovie: MutableLiveData<MoviesModel.Result> = MutableLiveData()
-//    var selectedMovie: LiveData<MoviesModel.Result> = _selectedMovie
-//
-//
-////    var selectedMovie by mutableStateOf(MoviesModel.Result())
-//
-//    fun setSelectedMovieValue(movie: MoviesModel.Result) {
-//        _selectedMovie.value = movie
-//    }
-
     private val _selectedMovie = MutableStateFlow(MoviesModel.Result())
     val selectedMovie = _selectedMovie.asStateFlow()
 
     fun setMovie(movie: MoviesModel.Result) {
-
         _selectedMovie.value = movie
-        Log.d("fasdfsdfsdfsdsdfsdfaafs", "setMovie: ${_selectedMovie.value}")
     }
 
     init {
@@ -60,22 +42,34 @@ class MainScreenViewModel (
     }
 
     fun getPopularMovies() {
-        getPopularMoviesListUseCase().onEach { viewState ->
-            when (viewState) {
-                is ViewState.Loading -> _popularMovies.value = MoviesViewState(isLoading = true)
-                is ViewState.Success -> _popularMovies.value = viewState.data?.let { MoviesViewState(movies = it) } ?: MoviesViewState()
-                is ViewState.Error -> _popularMovies.value = MoviesViewState(error = viewState.message)
+        if (_popularMovies.value.movies.results.isEmpty()) {
+            Log.d(
+                "checkingetpopularmovies",
+                "getPopularMovies: ${_popularMovies.value.movies.results}"
+            )
+            getPopularMoviesListUseCase().onEach { viewState ->
+                when (viewState) {
+                    is ViewState.Loading -> _popularMovies.value = MoviesViewState(isLoading = true)
+                    is ViewState.Success -> _popularMovies.value =
+                        viewState.data?.let { MoviesViewState(movies = it) } ?: MoviesViewState()
 
-            }
-        }.launchIn(viewModelScope)
+                    is ViewState.Error -> _popularMovies.value =
+                        MoviesViewState(error = viewState.message)
+
+                }
+            }.launchIn(viewModelScope)
+        }
     }
 
     fun getUpcomingMovies() {
         getUpcomingMoviesListUseCase().onEach { viewState ->
             when (viewState) {
                 is ViewState.Loading -> _upcomingMovies.value = MoviesViewState(isLoading = true)
-                is ViewState.Success -> _upcomingMovies.value = viewState.data?.let { MoviesViewState(movies = it) } ?: MoviesViewState()
-                is ViewState.Error -> _upcomingMovies.value = MoviesViewState(error = viewState.message)
+                is ViewState.Success -> _upcomingMovies.value =
+                    viewState.data?.let { MoviesViewState(movies = it) } ?: MoviesViewState()
+
+                is ViewState.Error -> _upcomingMovies.value =
+                    MoviesViewState(error = viewState.message)
 
             }
         }.launchIn(viewModelScope)
@@ -85,7 +79,9 @@ class MainScreenViewModel (
         getTVShowsListUseCase().onEach { viewState ->
             when (viewState) {
                 is ViewState.Loading -> _tvShows.value = MoviesViewState(isLoading = true)
-                is ViewState.Success -> _tvShows.value = viewState.data?.let { MoviesViewState(movies = it) } ?: MoviesViewState()
+                is ViewState.Success -> _tvShows.value =
+                    viewState.data?.let { MoviesViewState(movies = it) } ?: MoviesViewState()
+
                 is ViewState.Error -> _tvShows.value = MoviesViewState(error = viewState.message)
 
             }
--- app/src/main/java/com/example/movieverse_compose/presentation/ui/MainScreen.kt
+++ app/src/main/java/com/example/movieverse_compose/presentation/ui/MainScreen.kt
@@ -1,7 +1,6 @@
 package com.example.movieverse_compose.presentation.ui
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,12 +10,10 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.movieverse_compose.common.sdp
@@ -27,27 +24,15 @@ import com.example.movieverse_compose.presentation.components.NavigationItem
 import com.example.movieverse_compose.presentation.components.OwlCarousal
 import com.example.movieverse_compose.presentation.components.TextView
 import com.example.movieverse_compose.ui.theme.backgroundColor
-import org.koin.androidx.compose.koinViewModel
 
 @SuppressLint("CoroutineCreationDuringComposition", "MutableCollectionMutableState")
 @Composable
 fun MainScreen(
-    navController: NavHostController = rememberNavController(),
-    viewModel: MainScreenViewModel
+    navController: NavHostController = rememberNavController(), viewModel: MainScreenViewModel
 ) {
     val popularMoviesState by viewModel.popularMovies.collectAsState()
     val upcomingMoviesState by viewModel.upcomingMovies.collectAsState()
     val tvShowsMoviesState by viewModel.tvShows.collectAsState()
-
-    var isPopularMoviesLoading by remember { mutableStateOf(true) }
-    var popularMoviesList by remember { mutableStateOf(listOf<MoviesModel.Result>()) }
-
-    var isUpcomingMoviesLoading by remember { mutableStateOf(true) }
-    var upcomingMoviesList by remember { mutableStateOf(listOf<MoviesModel.Result>()) }
-
-    var isTvShowsLoading by remember { mutableStateOf(true) }
-    var tvShowsList by remember { mutableStateOf(listOf<MoviesModel.Result>()) }
-
 
     Column(
         modifier = Modifier
@@ -56,56 +41,29 @@ fun MainScreen(
             .background(color = backgroundColor)
             .padding(top = 44.sdp, bottom = 12.sdp)
     ) {
-        //Top Movies Row
+        //Popular Movies Row
         TextView(
             Modifier.padding(start = 16.sdp), text = "MovieVerse", textSize = 18, isTextBold = true
         )
-
-        OwlCarousal(isLoadingData = isPopularMoviesLoading, popularMoviesList = popularMoviesList){ selectedMovie->
+        OwlCarousal(
+            isLoadingData = popularMoviesState.isLoading,
+            popularMoviesList = popularMoviesState.movies.results.takeIf { it.isNotEmpty() }
+                ?.subList(0, 6) ?: emptyList()
+        ) { selectedMovie ->
             viewModel.setMovie(selectedMovie)
             navController.navigate(NavigationItem.Details.route)
-        }
-
-        if (popularMoviesState.isLoading) {
-            //its loading
-            isPopularMoviesLoading = true
-        } else if (popularMoviesState.error?.isNotEmpty() == true) {
-            //its an error
-            isPopularMoviesLoading = true
-        } else {
-            if (popularMoviesState.movies.results.isNotEmpty()) {
-                //Popular Movies Row
-                isPopularMoviesLoading = false
-                popularMoviesList = popularMoviesState.movies.results.subList(0,6)
-            } else {
-                isPopularMoviesLoading = true
-            }
         }
 
         //Upcoming Movies Row
         TextView(
-            Modifier.padding(start = 16.sdp ),
-            text = "Upcomings",
-            textSize = 14,
-            isTextBold = true
+            Modifier.padding(start = 16.sdp), text = "Upcomings", textSize = 14, isTextBold = true
         )
 
-        MoviesRow(isLoadingData = isUpcomingMoviesLoading, moviesList = upcomingMoviesList){ selectedMovie->
+        MoviesRow(isLoadingData = upcomingMoviesState.isLoading,
+            moviesList = upcomingMoviesState.movies.results.takeIf { it.isNotEmpty() }
+                ?: emptyList()) { selectedMovie ->
             viewModel.setMovie(selectedMovie)
             navController.navigate(NavigationItem.Details.route)
-        }
-
-        if (upcomingMoviesState.isLoading) {
-            isUpcomingMoviesLoading = true
-        } else if (upcomingMoviesState.error?.isNotEmpty() == true) {
-            isUpcomingMoviesLoading = true
-        } else {
-            if (upcomingMoviesState.movies.results.isNotEmpty()) {
-               isUpcomingMoviesLoading = false
-               upcomingMoviesList = upcomingMoviesState.movies.results.subList(0,6)
-            } else {
-                isUpcomingMoviesLoading = true
-            }
         }
 
         //TV Shows Row
@@ -116,22 +74,11 @@ fun MainScreen(
             isTextBold = true
         )
 
-        MoviesRow(isLoadingData = isTvShowsLoading, moviesList = tvShowsList){selectedMovie->
+        MoviesRow(isLoadingData = tvShowsMoviesState.isLoading,
+            moviesList = tvShowsMoviesState.movies.results.takeIf { it.isNotEmpty() }
+                ?: emptyList()) { selectedMovie ->
             viewModel.setMovie(selectedMovie)
             navController.navigate(NavigationItem.Details.route)
-        }
-
-        if (tvShowsMoviesState.isLoading) {
-            isTvShowsLoading = true
-        } else if (tvShowsMoviesState.error?.isNotEmpty() == true) {
-            isTvShowsLoading = true
-        } else {
-            if (tvShowsMoviesState.movies.results.isNotEmpty()) {
-                isTvShowsLoading = false
-                tvShowsList = tvShowsMoviesState.movies.results.subList(0,6)
-            } else {
-                isTvShowsLoading = true
-            }
         }
 
     }
